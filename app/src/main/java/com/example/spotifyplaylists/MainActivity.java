ff81474cea75331bcaaebaa7ab8efab38593fb29
Tom Frey <tom.frey@informatik.hu-berlin.de>
2019-04-27 03:08:17 +0200

    add spotify auth library

--- app/src/main/java/com/example/spotifyplaylists/MainActivity.java
+++ app/src/main/java/com/example/spotifyplaylists/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.spotifyplaylists;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String CLIENT_ID = "2728f152d53b42f1a29e3a967f34a02f";
+    public static final String REDIRECT_URI = "spotifyplaylists://callback";
+    public static final int AUTH_TOKEN_REQUEST_CODE = 2019;
+
+    private String mAccessToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onGetUserProfileClicked(View view) {
+        if (mAccessToken == null) {
+            Log.e("MainActivity", "Error: mAccessToken is null - please sign in first");
+            return;
+        }
+
+        Log.d("MainActivity", "Todo");
+
+        // Todo call playlistapi
+    }
+
+    public void onRequestTokenClicked(View view) {
+        final AuthenticationRequest request = getAuthenticationRequest(AuthenticationResponse.Type.TOKEN);
+        AuthenticationClient.openLoginActivity(this, AUTH_TOKEN_REQUEST_CODE, request);
+    }
+
+    private AuthenticationRequest getAuthenticationRequest(AuthenticationResponse.Type type) {
+        return new AuthenticationRequest.Builder(CLIENT_ID, type, getRedirectUri().toString())
+                .setShowDialog(false)
+                .setScopes(new String[]{"playlist-read-private"})
+                .build();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        final AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, data);
+
+        if (AUTH_TOKEN_REQUEST_CODE == requestCode) {
+            mAccessToken = response.getAccessToken();
+            updateTokenView();
+        }
+    }
+
+    private void updateTokenView() {
+        final TextView tokenView = findViewById(R.id.token_textView);
+        tokenView.setText(String.format("AccessToken: %s", mAccessToken));
+    }
+
+    private Uri getRedirectUri() {
+        return new Uri.Builder()
+                .scheme(getString(R.string.com_spotify_sdk_redirect_scheme))
+                .authority(getString(R.string.com_spotify_sdk_redirect_host))
+                .build();
+    }
+
 }
